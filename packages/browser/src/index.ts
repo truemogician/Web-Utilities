6ec54d7f205f9cd2a9b899b2d752e3781f1c88b1
true_mogician <truemogician@gmail.com>
2022-11-13 03:10:55 +0800

    feat(browser): implement text selection

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -2,4 +2,5 @@ export * from "./create-element";
 export * from "./indexed-db";
 export * from "./request-throttler";
 export * from "./scroll-throttler";
+export * from "./select-text";
 export * from "./storage";
---
+++ packages/browser/src/select-text.ts
@@ -0,0 +1,30 @@
+export interface TextWithOffset {
+	text: Text;
+
+	offset: number;
+}
+
+export function selectText(node: Node): void;
+export function selectText(node: Text, start: number, end?: number): void;
+export function selectText(start: Text | TextWithOffset, end: Text | TextWithOffset): void;
+export function selectText(param1: Text | Node | TextWithOffset, param2?: number | Text | TextWithOffset, param3?: number): void {
+	const range = document.createRange();
+	if (arguments.length == 1 && param1 instanceof Node)
+		range.selectNodeContents(param1);
+	else if (param1 instanceof Text && typeof param2 == "number") {
+		const end = typeof param3 == "number" ? param3 : param1.length;
+		range.setStart(param1, param2);
+		range.setEnd(param1, end);
+	}
+	else if (param2 && typeof param2 != "number") {
+		const start = param1 instanceof Text ? { text: param1, offset: 0 } : param1 as TextWithOffset;
+		const end = param2 instanceof Text ? { text: param2, offset: param2.length } : param2;
+		range.setStart(start.text, start.offset);
+		range.setEnd(end.text, end.offset);
+	}
+	else
+		throw new Error("Invalid arguments");
+	const selection = window.getSelection()!;
+	selection.removeAllRanges();
+	selection.addRange(range);
+}
