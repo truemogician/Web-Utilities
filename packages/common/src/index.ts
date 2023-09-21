9dc68d2bad0cb961c8f2d1ff54f3d2800ab2f82f
true_mogician <truemogician@gmail.com>
2023-09-21 18:13:39 +0800

    feat(common): add function `hashCode`
    The implementation is borrowed from [bryc](https://github.com/bryc)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,3 @@
 export { formatDate, formatTimezone, patterns as datePatterns, i18n as dateI18nSettings } from "./format-date";
+export { hashCode } from "./hash-code";
 export { createThrottledFetch, type ThrottledFetch, type ThrottleConfig } from "./throttled-fetch";
